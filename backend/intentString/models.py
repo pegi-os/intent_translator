33a11d436cda7c909ed6493ae987a26898130509
fkhl1234 <fkhl1234@gmail.com>
2025-11-28 13:52:34 +0900

    temp

--- backend/intentString/models.py
+++ backend/intentString/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class IntentString(models.Model):
+class NaturalIntent(models.Model):
       user = models.CharField(max_length=20)
       intent = models.CharField(max_length=50)
       timestamp = models.DateTimeField()
 
       def __str__(self):
             return self.user + ":" + self.intent
+      
+
+class NetworkIntent(models.Model):
+      name = models.CharField(max_length=100, null=True, blank=True)
+      mac_address = models.CharField(max_length=17, null=True, blank=True)
+
+      ipv4_start = models.GenericIPAddressField(protocol='IPv4', null=True, blank=True)
+      ipv4_end = models.GenericIPAddressField(protocol='IPv4', null=True, blank=True)
+      ipv6_start = models.GenericIPAddressField(protocol='IPv6', null=True, blank=True)
+      ipv6_end = models.GenericIPAddressField(protocol='IPv6', null=True, blank=True)
+
+      def __str__(self):
+            return self.name + "/" + self.mac_address + self.ipv4_start + "~" + self.ipv4_end + "/" + self.ipv6_start + "~" + self.ipv6_end
+
